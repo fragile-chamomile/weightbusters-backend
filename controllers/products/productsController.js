c6a0655fa7d261d69d26cad866eab89ac65befb5
Dmitrii Shapovalov <87037406+Kronord@users.noreply.github.com>
2022-07-10 13:06:32 +0300

    Bag fix

--- controllers/products/productsController.js
+++ controllers/products/productsController.js
@@ -1,21 +1,16 @@
 const { Product } = require("../../models/product");
-const { errorOrResponce } = require("../../error");
 
-const getProductsFromQueryParam = async (req, res, next) => {
-  try {
-    const { query } = req.params;
-    const products = await Product.find({
-      "title.ua": { $regex: `${query}`, $options: "i" },
-    });
-    if (products.length === 0) {
-      return res.json({ message: "No matches found" });
-    } else if (!products) {
-      return res.status(400).json({status: 400, body: "Bad Request"});
-    }
-    res.status(200).json(products);
-  } catch (err) {
-    next(err);
+const getProductsFromQueryParam = async (req, res) => {
+  const { query } = req.params;
+  const products = await Product.find({
+    "title.ua": { $regex: `${query}`, $options: "i" },
+  });
+  if (products.length === 0) {
+    return res.json({ message: "No matches found" });
+  } else if (!products) {
+    return res.status(400).json({ status: 400, body: "Bad Request" });
   }
+  res.status(200).json(products);
 };
 
 module.exports = { getProductsFromQueryParam };
--- routes/api/product.js
+++ routes/api/product.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { getProductsFromQueryParam } = require("../../controllers");
+const { ctrlWrapper } = require("../../middlewares");
 const { auth } = require("../../middlewares");
 
-router.post("/:query", auth, getProductsFromQueryParam);
+router.post("/:query", auth, ctrlWrapper(getProductsFromQueryParam));
 
 module.exports = router;
