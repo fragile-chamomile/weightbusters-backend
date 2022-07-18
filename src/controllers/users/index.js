6d569bc0096bfbf32bc51d676ddd67e68a53d260
Dmitrii Shapovalov <87037406+Kronord@users.noreply.github.com>
2022-07-18 22:37:46 +0300

    Add new route

--- src/controllers/users/index.js
+++ src/controllers/users/index.js
@@ -1,9 +1,11 @@
 const verifyEmail = require("./verifyEmail");
 const reVerification = require("./reVerification");
 const getCurrent = require("./getCurrent");
+const getUserInfo = require("./getUserInfo");
 
 module.exports = {
 	verifyEmail,
 	reVerification,
 	getCurrent,
+	getUserInfo,
 };
